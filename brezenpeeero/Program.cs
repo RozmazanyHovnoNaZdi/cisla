292bcbb256354c9873983321be6420f63356db22
Jakub Matějka <jakub.matejka@educhem.cz>
2022-06-21 09:23:13 +0200

    Přidejte soubory projektu.

---
+++ brezenpeeero/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("input first number: ");
+int redbull = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("input second number: ");
+int monster = Convert.ToInt32(Console.ReadLine());
+
+
+if (redbull > monster)
+{
+    Console.WriteLine($"Číslo {redbull} je větší než číslo {monster}");
+}
+else
+{
+    Console.WriteLine($"Číslo {monster} je větší než číslo {redbull}");
+}
